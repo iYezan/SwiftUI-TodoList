3e246be10edbc427c3113e4754c4643c776ef9f1
Yezan Ahmed <hashir_hush@hotmail.co.uk>
2022-01-07 23:23:51 +0000

    P1: This project uses MVVM architecture and UserDefaults to persist data.

--- TodoList/Screens/Views/Main/View/ContentView.swift
+++ TodoList/Screens/Views/Main/View/ContentView.swift
@@ -28,7 +28,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        
         NavigationView {
             ContentView()
         }
